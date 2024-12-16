c3d515acc95d93a6737b5dca2c6b9bca7eddfa03
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-12-17 01:17:04 +0600

    updated

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 import { useAppContext } from "@/context";
 import Image from "next/image";
 import { useState } from "react";
 
 const Home = () => {
-   const { state } = useAppContext();
-  const [outputImage, setOutputImage] = useState<string | null>(null);
+  const { state } = useAppContext();
+  const [outputImage, setOutputImage] = useState([]);
   const [imagePrompt, setImagePrompt] = useState<string | null>(null);
   const [isSubmited, setIsSubmited] = useState<boolean>(false);
+
+  console.log(outputImage);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -29,7 +31,7 @@ const Home = () => {
       const data = await response.json();
 
       if (response.ok) {
-        setOutputImage(data.url);
+        setOutputImage(data);
         setIsSubmited(false);
       } else {
         alert(data.error || "An unexpected error occurred. Please try again.");
@@ -48,15 +50,21 @@ const Home = () => {
       <div className="mx-auto flex flex-col justify-center items-center gap-9">
         {outputImage ? (
           <>
-            <h1 className="text-2xl mt-6">Your Generated Image</h1>
-            <div className="bg-indigo-300 ">
-              <Image
-                src={outputImage}
-                alt=""
-                width={1920}
-                height={1080}
-                className="object-cover h-[600px] w-[961px] rounded-lg"
-              />
+            <h1 className="text-xl mt-6">Your Generated Image</h1>
+            <div className="w-full flex flex-wrap gap-6 justify-center items-center">
+              {outputImage.map((img, index) => (
+                <div key={index} className="bg-indigo-300 ">
+                  <Image
+                    src={img}
+                    alt=""
+                    width={state.size.width / 4}
+                    height={state.size.height / 4}
+                    className={`object-cover h-[${state.size.width / 4}px] w-[${
+                      state.size.width / 4
+                    }px] rounded-lg`}
+                  />
+                </div>
+              ))}
             </div>
           </>
         ) : (
@@ -92,3 +100,98 @@ const Home = () => {
 };
 
 export default Home;
+
+// "use client"
+// import { useAppContext } from "@/context";
+// import Image from "next/image";
+// import { useState } from "react";
+
+// const Home = () => {
+//    const { state } = useAppContext();
+//   const [outputImage, setOutputImage] = useState<string | null>(null);
+//   const [imagePrompt, setImagePrompt] = useState<string | null>(null);
+//   const [isSubmited, setIsSubmited] = useState<boolean>(false);
+
+//   const handleSubmit = async (e: any) => {
+//     e.preventDefault();
+//     setIsSubmited(true);
+//     if (!navigator.onLine) {
+//       alert("No internet connection. Try later.");
+//       return;
+//     }
+//     setOutputImage("");
+//     try {
+//       const response = await fetch("/api/image", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ prompt: imagePrompt, state: state }),
+//       });
+
+//       const data = await response.json();
+
+//       if (response.ok) {
+//         setOutputImage(data.url);
+//         setIsSubmited(false);
+//       } else {
+//         alert(data.error || "An unexpected error occurred. Please try again.");
+//         setIsSubmited(false);
+//       }
+//     } catch (error) {
+//       console.error("Error fetching image:", error);
+//       alert(
+//         "Failed to fetch image. Please check your connection and try again."
+//       );
+//     }
+//   };
+
+//   return (
+//     <div className="w-5/6 py-6">
+//       <div className="mx-auto flex flex-col justify-center items-center gap-9">
+//         {outputImage ? (
+//           <>
+//             <h1 className="text-2xl mt-6">Your Generated Image</h1>
+//             <div className="bg-indigo-300 ">
+//               <Image
+//                 src={outputImage}
+//                 alt=""
+//                 width={1920}
+//                 height={1080}
+//                 className="object-cover h-[600px] w-[961px] rounded-lg"
+//               />
+//             </div>
+//           </>
+//         ) : (
+//           <div className="text-2xl text-center mt-60">
+//             Enter your prompt and hit generate!
+//           </div>
+//         )}
+//         <form
+//           onSubmit={handleSubmit}
+//           className="flex gap-6 justify-center items-center"
+//         >
+//           {/* PROMPT INPUT */}
+//           <div className="flex flex-col gap-1">
+//             <input
+//               type="text"
+//               placeholder="Eter your prompt"
+//               onChange={(e) => setImagePrompt(e.target.value)}
+//               className="sm:w-[450px] md:w-[600px] xl:w-[850px] bg-gray-950 border border-gray-700 outline-gray-700 rounded-md py-2 px-4"
+//             />
+//           </div>
+//           <button
+//             disabled={isSubmited}
+//             className={`${
+//               isSubmited ? "bg-gray-500" : "bg-teal-500 hover:bg-teal-600"
+//             } w-fit py-1.5 px-4 rounded-md text-white transition-all ease-linear duration-150`}
+//           >
+//             {isSubmited ? "Generating..." : "Generate"}
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -3,17 +3,28 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    const { prompt, state }: { prompt: string, state:State } = await request.json();
+    const { prompt, state }: { prompt: string; state: State } =
+      await request.json();
 
-    console.log("Received prompt:", prompt); // This should now log the correct value
-    console.log(state); 
+    // console.log("Received prompt:", prompt); // This should now log the correct value
+    // console.log(state);
 
-    const randomSeed = Math.floor(Math.random() * 100000000) + 1;
-    const imageURL = `https://image.pollinations.ai/prompt/${encodeURIComponent(
-      prompt
-    )}?seed=${randomSeed}&width=${state.size.width}&height=${state.size.height}&nologo=True&filetype=png`;
+    const imagesArray = [];
 
-    return NextResponse.json({ url: imageURL });
+    for (let i = 0; i < state.numberOfImage; i++) {
+      const randomSeed = Math.floor(Math.random() * 100000000) + 1;
+      const imageURL = `https://image.pollinations.ai/prompt/${encodeURIComponent(
+        prompt
+      )}?seed=${randomSeed}&width=${state.size.width}&height=${
+        state.size.height
+      }&nologo=True&filetype=png`;
+
+      imagesArray.push(imageURL);
+    }
+    console.log(imagesArray);
+    
+
+    return NextResponse.json(imagesArray);
   } catch (error) {
     console.error("Error generating image URL:", error);
     return NextResponse.json(
@@ -23,4 +34,26 @@ export async function POST(request: NextRequest) {
   }
 }
 
+// import { State } from "@/context";
+// import { NextRequest, NextResponse } from "next/server";
 
+// export async function POST(request: NextRequest) {
+//   try {
+//     const { prompt, state }: { prompt: string, state:State } = await request.json();
+
+//     console.log("Received prompt:", prompt); // This should now log the correct value
+
+//     const randomSeed = Math.floor(Math.random() * 100000000) + 1;
+//     const imageURL = `https://image.pollinations.ai/prompt/${encodeURIComponent(
+//       prompt
+//     )}?seed=${randomSeed}&width=${state.size.width}&height=${state.size.height}&nologo=True&filetype=png`;
+
+//     return NextResponse.json({ url: imageURL });
+//   } catch (error) {
+//     console.error("Error generating image URL:", error);
+//     return NextResponse.json(
+//       { error: "Failed to generate image URL." },
+//       { status: 500 }
+//     );
+//   }
+// }
